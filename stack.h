eeba78a09388f8a6071bc0ee52e653437cdf3c26
arrij46 <i220755@nu.edu.pk>
2024-05-12 15:15:41 +0500

    stack header added

---
+++ stack.h
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 1000
+
+typedef struct
+{
+	int x, y;
+} Pair;
+
+void PairInit(Pair *p)
+{
+	p->x=-1;
+	p->y=-1;
+}
+
+
+
+typedef struct
+{
+	Pair List[MAX_SIZE];
+	int top;
+} Stack;
+
+void StackInit(Stack *s)
+{
+	s->top = -1;
+	//doesnt matter
+	PairInit(&s->List[0]);
+}
+
+
+int isFull(Stack *s)
+{
+	if (s->top == MAX_SIZE - 1)
+		return 1;
+	else
+		return 0;
+}
+
+int isEmpty(Stack *s)
+{
+	if (s->top == -1)
+		return 1;
+	else
+		return 0;
+}
+
+void push(Stack *s, int x, int y)
+{
+	if (isFull(s))
+	{
+		printf("Stack overflow\n");
+		return;
+	}
+	s->top++;
+	s->List[s->top].x = x;
+	s->List[s->top].y = y;
+}
+
+void pop(Stack *s)
+{
+	if (isEmpty(s))
+	{
+		printf("Stack underflow\n");
+		return;
+	}
+	s->top--;
+}
+
+Pair front(Stack *s)
+{
+	if (isEmpty(s))
+	{
+		printf("Stack is empty\n");
+		exit(EXIT_FAILURE);
+	}
+	return s->List[s->top]; // Return the top element of the stack
+}
+
+void clear(Stack *s)
+{
+	s->top = -1; // Reset the top index to indicate an empty stack
+}
